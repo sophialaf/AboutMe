997ab4119f2e15cc3ee07cea942d4eccd777c3f8
sophialaf <102073652+sophialaf@users.noreply.github.com>
2023-04-22 15:21:36 +0200

    code works, but pictures are ugly

---
+++ RandomPictures/index.js
@@ -0,0 +1,28 @@
+const express = require('express'); 
+const app = express();
+const port = 8080;
+const path = require('path') 
+
+let server = app.listen(port, function () {
+    console.log("App server is running on port", port);
+    console.log("to end press Ctrl + C");
+});
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+
+const list = [
+    "https://picsum.photos/id/31/3264/4912",
+    "https://picsum.photos/id/33/5000/3333",
+    "https://picsum.photos/id/30/1280/901",
+    "https://picsum.photos/id/43/1280/831",
+    "https://picsum.photos/id/53/1280/1280"
+];
+app.get('/img', function (req, res) {
+    let i = Math.floor(Math.random() * list.length);
+    let url = list[i];
+    res.send(url);
+});
